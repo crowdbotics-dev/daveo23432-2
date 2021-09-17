51c2d8b32bda2bb74ebe3b042916cbd49c824925
Crowdbotics <engineering@crowdbotics.com>
2021-09-17 15:09:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "daveo23432_2Identifier",
+  url: "https://daveo23432_2Identifier.botics.co",
+  api: "https://daveo23432_2Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
